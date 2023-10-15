44abfd975f3b2c77514ba0a276d276db3eded159
Your Name <you@example.com>
2023-10-16 06:31:57 +0900

    デバック用

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 19:03:26 by anonymous         #+#    #+#             */
-/*   Updated: 2023/10/15 19:05:33 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/10/16 06:31:18 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,22 @@ static int	initialize(t_stack *a, t_stack *b, int argc, char *argv[])
 	}
 	return (TRUE);
 }
+
+// #include <stdio.h>
+// void	print_stack(t_stack *stack)
+// {
+// 	uint32_t	i;
+
+// 	printf("stack_%c\n", stack->label);
+
+// 	i = 0;
+// 	while (i < stack->len)
+// 	{
+// 		printf("%ld ", stack->buffer[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// }
 
 int	main(int argc, char *argv[])
 {
