2cddbbae56d5bc98ff04c1dd52a2d7f66a248956
Your Name <you@example.com>
2023-10-17 22:54:02 +0900

    フェーズ３

---
+++ ft_phase3.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_phase3.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 22:46:55 by anonymous         #+#    #+#             */
+/*   Updated: 2023/10/17 22:53:01 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_phase3.h"
+
+#include <stddef.h>
+#include "ft_operation.h"
+#include "libft.h"
+
+void	ft_phase3(t_stack *stack)
+{
+	int64_t		index;
+	t_operation	operation;
+
+	index = ft_stack_find(stack, stack->min);
+	ft_bzero(&operation, sizeof(operation));
+	if (index <= stack->label / 2)
+		operation.ra = index;
+	else
+		operation.rra = stack->len - index;
+	ft_stack_batch(stack, NULL, operation);
+}
