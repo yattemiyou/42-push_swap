bb61c32a8ef906dfd64f635fe69abfe7575b25d1
Your Name <you@example.com>
2023-10-17 22:36:20 +0900

    スタック操作のバッチ処理

--- ft_operation.h
+++ ft_operation.h
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 15:25:07 by anonymous         #+#    #+#             */
-/*   Updated: 2023/10/17 19:56:46 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/10/17 22:17:46 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,20 @@
 
 # include "ft_stack.h"
 
-void	ft_stack_rotate(t_stack *stack);
-void	ft_stack_rrotate(t_stack *stack);
-void	ft_stack_swap(t_stack *stack);
+typedef struct s_operation
+{
+	int	ra;
+	int	rb;
+	int	rr;
+	int	rra;
+	int	rrb;
+	int	rrr;
+}	t_operation;
+
+void	ft_stack_batch(t_stack *a, t_stack *b, t_operation operation);
+void	ft_stack_push(t_stack *dest, t_stack *src);
+void	ft_stack_rotate(t_stack *stack, int display);
+void	ft_stack_rrotate(t_stack *stack, int display);
+void	ft_stack_swap(t_stack *stack, int display);
 
 #endif
