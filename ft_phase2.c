dc73738720389e3902587c847bc016a1faeb027c
Your Name <you@example.com>
2023-10-21 08:20:46 +0900

    挿入位置の取得

--- ft_phase2.c
+++ ft_phase2.c
@@ -12,6 +12,21 @@
 
 #include "ft_phase2.h"
 
+static uint32_t	get_insertable_index(t_stack *stack, int n)
+{
+	uint32_t	prev;
+	uint32_t	i;
+
+	if (n < stack->min || n > stack->max)
+		return (ft_stack_find(stack, stack->min));
+	prev = stack->len - 1;
+	i = 0;
+	while (stack->buffer[prev] > n || n > stack->buffer[i])
+		prev = ++i - 1;
+	return (i);
+}
+
+
 void	ft_phase2(t_stack *a, t_stack *b)
 {
 	(void)a;
